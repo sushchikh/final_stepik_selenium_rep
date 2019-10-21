6bec026ab530aefd858b9304626f8c3d4b13dce4
sushchikh <a.sushchikh@gmail.com>
2019-10-21 21:57:03 +0300

    add test.py for try some features of pyt3.8, add few string in test_main_page

---
+++ test.py
@@ -0,0 +1,3 @@
+import sys
+x = 'farhadmalik'
+print(sys.getsizeof(x))
--- test_main_page.py
+++ test_main_page.py
@@ -1,5 +1,19 @@
+link = "http://selenium1py.pythonanywhere.com/"
+
+
+def go_to_login_page(browser):
+    link = browser.find_element_by_css_selector("#login_link")
+    link.click()
+
+
 def test_guest_can_go_to_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
     browser.get(link)
-    login_link = browser.find_element_by_css_selector("#login_link")
-    login_link.click()
+    go_to_login_page(browser)
+
+
+# def test_add_to_cart(browser):
+#     page = ProductPage(url="", browser)   # инициализируем объект Page Object
+#     page.open()                           # открываем страницу в браузере
+#     page.should_be_add_to_cart_button()   # проверяем что есть кнопка добавления в корзину
+#     page.add_product_to_cart()            # жмем кнопку добавить в корзину
+#     page.should_be_success_message()      # проверяем что есть сообщение с нужным текстом
