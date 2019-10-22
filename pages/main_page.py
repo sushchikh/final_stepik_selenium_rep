8409251d838eb5c5cbaae015cfb544351fe9fa43
sushchikh <a.sushchikh@gmail.com>
2019-10-22 19:24:07 +0300

    add reference to login page it test

--- pages/main_page.py
+++ pages/main_page.py
@@ -1,6 +1,8 @@
 from .base_page import BasePage
 from selenium.webdriver.common.by import By
 from .locators import MainPageLocators
+from .login_page import LoginPage
+
 
 class MainPage(BasePage):
     def should_be_login_link(self):
@@ -9,4 +11,5 @@ class MainPage(BasePage):
     def go_to_login_page(self):
         link = self.browser.find_element(*MainPageLocators.LOGIN_LINK)
         link.click()
+        # return LoginPage(browser=self.browser, url=self.browser.current_url)
 
