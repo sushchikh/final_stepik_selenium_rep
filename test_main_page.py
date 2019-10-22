18845aa3e08f4c0dfca6e39ad5cb705d2ac2eafb
sushchikh <a.sushchikh@gmail.com>
2019-10-22 17:13:49 +0300

    add new test for the loging page

--- test_main_page.py
+++ test_main_page.py
@@ -1,9 +1,11 @@
 from .pages.main_page import MainPage
-link = "http://selenium1py.pythonanywhere.com/"
+from .pages.login_page import LoginPage
+# link = "http://selenium1py.pythonanywhere.com/"
 
 
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
+    # link = "http://selenium1py.pythonanywhere.com/ru/catalogue/the-shellcoders-handbook_209/?promo=midsummer"
     page = MainPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
     page.open()                      # открываем страницу
     page.go_to_login_page()          # выполняем метод страницы - переходим на страницу логина
@@ -11,6 +13,15 @@ def test_guest_can_go_to_login_page(browser):
 
 def test_guest_should_see_login_link(browser):
     link = "http://selenium1py.pythonanywhere.com/"
+    # link = "http://selenium1py.pythonanywhere.com/ru/catalogue/the-shellcoders-handbook_209/?promo=midsummer"
     page = MainPage(browser, link)
     page.open()
     page.should_be_login_link()
+
+def test_login_page_forms(browser):
+    link = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
+    page = LoginPage(browser, link)
+    page.open()
+    page.should_be_login_url()
+    page.should_be_login_form()
+    page.should_be_register_form()
